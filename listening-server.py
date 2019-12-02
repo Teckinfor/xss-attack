8ddabe9c403ccc558bffe31045daf8af02662621
groumache <51001261+groumache@users.noreply.github.com>
2019-12-02 11:07:42 +0100

    Update listening-server.py => make it a proper http server

--- listening-server.py
+++ listening-server.py
@@ -1,32 +1,10 @@
-import socket
+import http.server
+import socketserver
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 12_000
+Handler = http.server.SimpleHTTPRequestHandler
 
-# Bind the socket to the port
-server_address = ('127.0.0.1', 12_000)
-print('Starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-
-    try:
-        print('connection from', client_address)
-
-        # receive the data
-        while True:
-            data = connection.recv(16)
-            if data:
-                print('received {!r}'.format(data))
-
-    finally:
-        # Clean up the connection
-        print("Closing current connection")
-        connection.close()
-
+with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    print(httpd.get_request())
+    httpd.handle_request()
